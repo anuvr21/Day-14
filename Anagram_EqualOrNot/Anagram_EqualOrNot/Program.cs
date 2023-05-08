f41769c0db132669e85f2827fb33884e2b9cfe59
anuvr21 <129931334+anuvr21@users.noreply.github.com>
2023-05-08 09:41:41 +0530

    Add files via upload

---
+++ Anagram_EqualOrNot/Anagram_EqualOrNot/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Anagram_EqualOrNot
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Enter first string:");
+            string str1 = Console.ReadLine();
+            Console.WriteLine("Enter second string:");
+            string str2 = Console.ReadLine(); ;
+            char[] ch1 = str1.ToLower().ToCharArray();
+            char[] ch2 = str2.ToLower().ToCharArray();
+            Array.Sort(ch1);
+            Array.Sort(ch2);
+            string val1 = new string(ch1);
+            string val2 = new string(ch2);
+
+            if (val1 == val2)
+            {
+                Console.WriteLine("Both the strings are Anagrams");
+            }
+            else
+            {
+                Console.WriteLine("Both the strings are not Anagrams");
+            }
+        }
+    }
+}
